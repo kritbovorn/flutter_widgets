bb2d39208e371a7f790ecac342d229128ee07b9e
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-02 10:35:22 +0700

    finish Dot Indicators

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -39,7 +40,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _pageController.addListener(() {
       setState(() {
-      _currentPageValue = _pageController.page!;
+        _currentPageValue = _pageController.page!;
       });
       debugPrint('Current Page Value is: ' + _currentPageValue.toString());
     });
@@ -57,33 +58,50 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         elevation: 0,
       ),
-      body: SizedBox(
-        height: _itemHeight,
-        child: PageView.builder(
-          controller: _pageController,
-          itemCount: 9,
-          itemBuilder: (context, index) => _carouselSlider(index),
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: _itemHeight,
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: 7,
+              itemBuilder: (context, index) => _carouselSlider(index),
+            ),
+          ),
+          DotsIndicator(
+            dotsCount: 7,
+            position: _currentPageValue,
+            decorator: DotsDecorator(
+              size: const Size.square(9.0),
+              activeSize: const Size(22.0, 9.0),
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Widget _carouselSlider(index) {
-
     Matrix4 matrix4 = Matrix4.identity();
 
     if (index == _currentPageValue.floor()) {
       var scale = 1 - ((_currentPageValue - index) * (1 - _scaleFraction));
       var position = _itemHeight * (1 - scale) / 2;
-      matrix4 = Matrix4.diagonal3Values(1, scale, 1)..setTranslationRaw(0, position, 0);
+      matrix4 = Matrix4.diagonal3Values(1, scale, 1)
+        ..setTranslationRaw(0, position, 0);
     } else if (index == _currentPageValue.floor() + 1) {
-      var scale = _scaleFraction + ((_currentPageValue - index + 1) * (1 - _scaleFraction));
+      var scale = _scaleFraction +
+          ((_currentPageValue - index + 1) * (1 - _scaleFraction));
       var position = _itemHeight * (1 - scale) / 2;
-      matrix4 = Matrix4.diagonal3Values(1, scale, 1)..setTranslationRaw(0, position, 0);
+      matrix4 = Matrix4.diagonal3Values(1, scale, 1)
+        ..setTranslationRaw(0, position, 0);
     } else {
       var scale = _scaleFraction;
       var position = _itemHeight * (1 - scale) / 2;
-      matrix4 = Matrix4.diagonal3Values(1, scale, 1)..setTranslationRaw(0, position, 0);
+      matrix4 = Matrix4.diagonal3Values(1, scale, 1)
+        ..setTranslationRaw(0, position, 0);
     }
     return Transform(
       transform: matrix4,
