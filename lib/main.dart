2f5534d0dea92fc8fb05088e1842d8be40b21dc7
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-02 08:47:55 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,98 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  final PageController _pageController = PageController(viewportFraction: 0.85);
+  final double _itemHeight = 260;
+  final double _scaleFraction = 0.8;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  double _currentPageValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+      _currentPageValue = _pageController.page!;
+      });
+      debugPrint('Current Page Value is: ' + _currentPageValue.toString());
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SizedBox(
+        height: _itemHeight,
+        child: PageView.builder(
+          controller: _pageController,
+          itemCount: 9,
+          itemBuilder: (context, index) => _carouselSlider(index),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _carouselSlider(index) {
+
+    Matrix4 matrix4 = Matrix4.identity();
+
+    if (index == _currentPageValue.floor()) {
+      var scale = 1 - ((_currentPageValue - index) * (1 - _scaleFraction));
+      var position = _itemHeight * (1 - scale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, scale, 1)..setTranslationRaw(0, position, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var scale = _scaleFraction + ((_currentPageValue - index + 1) * (1 - _scaleFraction));
+      var position = _itemHeight * (1 - scale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, scale, 1)..setTranslationRaw(0, position, 0);
+    } else {
+      var scale = _scaleFraction;
+      var position = _itemHeight * (1 - scale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, scale, 1)..setTranslationRaw(0, position, 0);
+    }
+    return Transform(
+      transform: matrix4,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          image: const DecorationImage(
+            image: AssetImage(
+              'assets/helmet.jpg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
